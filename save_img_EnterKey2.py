d0b45238398c8dbc2c8bb03b4dfd9793cbb3cb2a
Jinny Kim <kyj20214@gmail.com>
2024-07-26 11:58:17 +0900

    pack 방식으로 가운데 정렬

--- save_img_EnterKey2.py
+++ save_img_EnterKey2.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import threading
 import tkinter as tk
 from tkinter import Label, Entry, messagebox, ttk, Button
 from PIL import Image, ImageTk
@@ -10,27 +9,22 @@ capture_image = None
 save_dir = "./save_img"
 cap = None
 previous_filename = None
-current_camera_index = 0
-lock = threading.Lock()
 
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
-
 def update_frame():
-    global capture_image, cap
-    with lock:
-        if cap is not None and cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
-                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                image = Image.fromarray(image)
-                image_tk = ImageTk.PhotoImage(image)
-                webcam_label.imgtk = image_tk
-                webcam_label.configure(image=image_tk)
-                capture_image = frame.copy()
-    webcam_label.after(10, update_frame)
-
+    global capture_image
+    if cap is not None and cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(image)
+            image_tk = ImageTk.PhotoImage(image)
+            webcam_label.imgtk = image_tk
+            webcam_label.configure(image=image_tk)
+            webcam_label.after(10, update_frame)
+            capture_image = frame.copy()
 
 def save_image(event=None):
     global capture_image, previous_filename
@@ -60,23 +54,23 @@ def save_image(event=None):
     else:
         messagebox.showwarning("No Image Captured", "캡처된 이미지가 없습니다")
 
-
-def change_camera():
-    global current_camera_index
-    selected_camera = int(camera_combobox.get())
-    if selected_camera != current_camera_index:
-        current_camera_index = selected_camera
-        root.after(0, init_camera, selected_camera)
-
-
-def init_camera(camera_index):
+def start_webcam():
     global cap
-    with lock:
-        if cap is not None:
-            cap.release()
-        cap = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
-    update_frame()
+    selected_cam = camera_combobox.get()
+    if selected_cam:
+        cap_index = int(selected_cam)
+        cap = cv2.VideoCapture(cap_index, cv2.CAP_DSHOW)
+        update_frame()
 
+def stop_webcam():
+    global cap
+    if cap is not None:
+        cap.release()
+        cap = None
+        webcam_label.config(image='')
+
+    control_frame.pack_forget()
+    init_control_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
 
 def find_cameras():
     index = 0
@@ -91,38 +85,44 @@ def find_cameras():
         index += 1
     return arr
 
-
 # GUI 생성
 root = tk.Tk()
 root.title("Webcam Capture")
 
+# 웹캠 비디오 출력 레이블
 webcam_label = Label(root)
-webcam_label.pack(side=tk.TOP)
+webcam_label.pack(side=tk.TOP, pady=10)
 
-control_frame = tk.Frame(root)
-control_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
+# 초기 제어 프레임
+init_control_frame = tk.Frame(root)
+init_control_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=10, pady=10)
 
-instructions_label = Label(control_frame, text="저장하고 싶은 파일명을 입력한 후 엔터로 사진을 저장합니다")
-instructions_label.grid(row=0, column=0, padx=5, pady=5)
+Label(init_control_frame, text="사용할 웹캠을 선택한 후 start를 누르세요").pack(pady=5)
 
-filename_entry = Entry(control_frame, width=20)
-filename_entry.grid(row=1, column=0, padx=5, pady=5)
-filename_entry.bind("<Return>", save_image)  # Enter 키를 눌렀을 때 save_image 함수 호출
-
-camera_combobox = ttk.Combobox(control_frame, values=find_cameras(), state="readonly")
-camera_combobox.grid(row=2, column=0, padx=5, pady=5)
+camera_combobox = ttk.Combobox(init_control_frame, values=find_cameras(), state="readonly")
+camera_combobox.pack(pady=5)
 camera_combobox.current(0)
 
-change_camera_button = Button(control_frame, text="Change Camera", command=change_camera)
-change_camera_button.grid(row=3, column=0, padx=5, pady=5)
+start_button = Button(init_control_frame, text="Start", command=lambda: [init_control_frame.pack_forget(),
+                                                                         control_frame.pack(side=tk.BOTTOM, fill=tk.X,
+                                                                                            padx=10, pady=10),
+                                                                         start_webcam()])
+start_button.pack(pady=5)
 
-# 기본 웹캠 초기화
-root.after(0, init_camera, int(camera_combobox.get()))
+# 제어 프레임
+control_frame = tk.Frame(root)
+
+instructions_label = Label(control_frame, text="저장하고 싶은 파일명을 입력한 후 엔터로 사진을 저장합니다")
+instructions_label.pack(pady=5)
+
+filename_entry = Entry(control_frame, width=50, justify='center')
+filename_entry.pack(pady=5)
+filename_entry.bind("<Return>", save_image)  # Enter 키를 눌렀을 때 save_image 함수 호출
+
+stop_button = Button(control_frame, text="Stop", command=stop_webcam)
+stop_button.pack(pady=5)
+
+# 프로그램 종료 시 웹캠 자원 해제
+root.protocol("WM_DELETE_WINDOW", lambda: [cap.release() if cap else None, root.destroy()])
 
 root.mainloop()
-
-# Release the webcam and close windows when the GUI is closed
-if cap is not None:
-    cap.release()
-cv2.destroyAllWindows()
-
